f2cf232604fc737f44fe9ea8d726086d13a4b003
Mercedes Walsh <sadieswalsh@gmail.com>
2019-11-23 12:18:28 -0800

    gitignore added more content

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 module.exports = app => {
+  // require statements (app)
   require('./excuseRoutes.js')(app)
   require('./eventRoutes.js')(app)
   require('./userRoutes.js')(app)
 }
-    // require statements (app)
-  
-  
