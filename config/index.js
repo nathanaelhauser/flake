f0fa2664fc692df39a567319fe576649a74d9acd
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-23 11:39:02 -0800

    Added .env

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize('mysql://root:groot@localhost/flake_db')
+module.exports = new Sequelize(process.env.NODE_ENV ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
