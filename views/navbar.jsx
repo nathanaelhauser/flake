3c2108d2d08ed754c9fcc11dd2552dc30796f27d
chbbs231 <atrobles231@gmail.com>
2019-11-23 11:47:30 -0800

    some changes

---
+++ views/navbar.jsx
@@ -0,0 +1,21 @@
+const React = require('react')
+require = ('./Layout')
+
+module.exports = props => {
+    <div class= "container">
+    <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+</div>
+}
