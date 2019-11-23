6db4091639943506090e5922a546f5b81ca4029f
Mercedes Walsh <sadieswalsh@gmail.com>
2019-11-23 12:48:52 -0800

    fixed model tables and commented for routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,18 +2,21 @@ const { User } = require('../models')
 
 module.exports = app => {
 
+  // GET all users (read)
   app.get('/users', (req, res) => {
     User.findAll()
       .then(users => res.json(users))
       .catch(e => console.error(e))
   })
 
+  // POST one user (create)
   app.post('/users', (req, res) => {
     User.create(req.body)
       .then(() => res.sendStatus(200))
       .catch(e => console.error(e))
   })
 
+  // PUT one user (update)
   app.put('/users/:id', (req, res) => {
     User.findOne({ where: { id: parseInt(req.params.id) } })
       .then(user => user.update({ isDone: !user.isDone}))
@@ -21,6 +24,7 @@ module.exports = app => {
       .catch(e => console.error(e))
   })
 
+  // DELETE one user (delete)
   app.delete('/users/:id', (req, res) => {
     User.findOne({ where: { id: parseInt(req.params.id) } })
       .then(user => user.destroy())
