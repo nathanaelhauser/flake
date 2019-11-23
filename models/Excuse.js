b6bdb81ab22123626cd1d1892017b455b29fbe74
Mercedes Walsh <sadieswalsh@gmail.com>
2019-11-23 11:35:25 -0800

    added models (excuse, event, user)

--- models/Excuse.js
+++ models/Excuse.js
@@ -2,9 +2,8 @@ module.exports = (sequelize, Model, DataTypes) => {
     class Excuse extends Model { }
 
     Excuse.init({
-        theExcuse: DataTypes.STRING(1234),
+        excuse: DataTypes.STRING(1234),
         severity: DataTypes.INTEGER,
-        used: DataTypes.BOOLEAN
         }, { sequelize, modelName: 'excuse' })
 
     return Excuse
--- models/index.js
+++ models/index.js
@@ -3,8 +3,12 @@ const sequelize = require('../config')
 
 // table variables, require statement, (sequelize, Model, DataTypes)
 const Excuse = require('./Excuse.js')(sequelize, Model, Datatypes)
+const Event = require('./Event.js')(sequelize, Model, DataTypes)
+const User = require('./User.js')(sequelize, Model, DataTypes)
 
 // associations if necessary
+User.hasMany(Event)
+Event.hasOne(Excuse)
 
 // module.exports = { table variables }
-module.exports = { Excuse }
+module.exports = { Excuse, Event, User }
