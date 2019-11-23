e37fd2c69241451e8fbfcefcc1debc5c26aa57d2
Maverick <maverick.schirmer@gmail.com>
2019-11-23 13:34:08 -0800

    changed class names

--- views/Layout/index.jsx
+++ views/Layout/index.jsx
@@ -10,7 +10,7 @@ module.exports = props => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/css/mdb.min.css" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
-    integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous"/>
+    integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossOrigin="anonymous"/>
             <title>Welcome to Flake</title>
         </head>
         <body>
--- views/home.jsx
+++ views/home.jsx
@@ -7,28 +7,28 @@ module.exports = props => {
     <Layout>
      
 <div class="dropdown-menu">
-  <form class="px-4 py-3">
-    <div class="form-group">
-      <label for="exampleDropdownFormEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
+  <form className="px-4 py-3">
+    <div className="form-group">
+      <label htmlFor="exampleDropdownFormEmail1">Email address</label>
+      <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
     </div>
-    <div class="form-group">
-      <label for="exampleDropdownFormPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
+    <div className="form-group">
+      <label htmlFor="exampleDropdownFormPassword1">Password</label>
+      <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
     </div>
-    <div class="form-group">
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
-        <label class="form-check-label" for="dropdownCheck">
+    <div className="form-group">
+      <div className="form-check">
+        <input type="checkbox" className="form-check-input" id="dropdownCheck"/>
+        <label className="form-check-label" htmlFor="dropdownCheck">
           Remember me
         </label>
       </div>
     </div>
-    <button type="submit" class="btn btn-primary">Sign in</button>
+    <button type="submit" className="btn btn-primary">Sign in</button>
   </form>
-  <div class="dropdown-divider"></div>
-  <a class="dropdown-item" href="#">Feeling Flakey? Sign up</a>
-  <a class="dropdown-item" href="#">Forgot password?</a>
+  <div className="dropdown-divider"></div>
+  <a className="dropdown-item" href="#">Feeling Flakey? Sign up</a>
+  <a className="dropdown-item" href="#">Forgot password?</a>
 </div>
     </Layout>
     )
