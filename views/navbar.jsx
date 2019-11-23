459af3ef6c1fd4a9c309f51498f6a0be793d494f
Mercedes Walsh <sadieswalsh@gmail.com>
2019-11-23 13:27:24 -0800

    CALANDER becomes CALENDAR

--- views/navbar.jsx
+++ views/navbar.jsx
@@ -20,7 +20,7 @@ module.exports = props => {
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Calander</a>
+            <a class="nav-link" href="#">Calendar</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Excuses</a>
