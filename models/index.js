7fcd4a7f807d5b69d0f3850e69b6cadde8fd71bd
Mercedes Walsh <sadieswalsh@gmail.com>
2019-11-23 10:42:23 -0800

    commented for future info in index files

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config')
+
+// table variables, require statement, (sequelize, Model, DataTypes)
+
+// associations if necessary
+
+// module.exports = { table variables }
