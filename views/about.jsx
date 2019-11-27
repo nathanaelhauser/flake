a4e2635af36d5d7ecad05282e8d36cc208935062
Paretooptimal22 <you.k2208@gmail.com>
2019-11-26 18:42:16 -0800

    added Card folder content

--- views/about.jsx
+++ views/about.jsx
@@ -1,17 +1,11 @@
 const React = require('react')
 const Layout = require('./Layout')
+const Card = require(`./Card`)
 
 module.exports = props => {
   return (
     <Layout>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1>Be a Flaker!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore quisquam accusamus recusandae, expedita omnis nostrum et. Sequi temporibus reiciendis deleniti tempore esse, corporis aliquam, delectus veritatis eligendi cupiditate quasi ipsum.</p>
-          </div>
-        </div>
-      </div>
+      
     </Layout>
     
   )
