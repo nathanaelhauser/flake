47759b4e199511d32cd31f4b76a78eec018f3bbc
Paretooptimal22 <you.k2208@gmail.com>
2019-11-26 19:38:34 -0800

    added About Card to About page

---
+++ views/AboutCard/index.jsx
@@ -0,0 +1,23 @@
+const React = require(`react`)
+
+module.exports = props => {
+  return(
+    <div className="container">
+      <div class="card card-cascade narrower">
+        <div class="view view-cascade overlay">
+          <img class="card-img-top" src={props.image} alt="Card image cap" />
+          <a>
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+        <div class="card-body card-body-cascade">
+          <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Culinary</h5>
+  <h4 class="font-weight-bold card-title">{props.name}</h4>
+  <p class="card-text">{props.description}</p>
+          <a class="btn btn-unique">Button</a>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
