fa5bdfb4a04a8b5b1f62b3f44b4536cca4211109
yokevi <you.k2208@gmail.com>
2019-12-03 21:11:33 -0800

    added footer to views

--- views/Calendar/index.jsx
+++ views/Calendar/index.jsx
@@ -1,12 +1,14 @@
 const React = require('react')
 const Layout = require('../Layout')
 const Navbar = require('../Navbar')
+const Footer = require('../Footer')
 
 module.exports = props =>{
     return (
         <Layout>
             <Navbar/>
 
+            <Footer/>
             </Layout>
     )
 }
