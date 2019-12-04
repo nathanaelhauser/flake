8b7693fa0fd94f583da5b391f7613c6df4931d72
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-12-03 19:35:45 -0800

    fixed navbar error

--- views/Navbar/index.jsx
+++ views/Navbar/index.jsx
@@ -1,58 +1,40 @@
 const React = require('react')
 
 module.exports = props => {
-  return(
+  return (
     <nav className="navbar navbar-light navbar-1 peach-gradient">
-    
-        <a className="navbar-brand" href="#">Flake</a>
-      
-        
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
-          aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-      
+
+      <a className="navbar-brand" href="#">Flake</a>
+
+
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
         aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-    
-    
+
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
+        aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+        
       <div className="collapse navbar-collapse" id="navbarSupportedContent15">
-    
+
+
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="./Home">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a id="calendarLink" className="nav-link" href="/calendar">Calender</a>
+            <a className="nav-link" href="./Calendar">Calendar</a>
           </li>
           <li className="nav-item">
-            <a id="excusesLink" className="nav-link" href="/excuses">Excuses</a>
+            <a className="nav-link" href="./Excuses">Excuses</a>
           </li>
           <li className="nav-item">
-            <a id="aboutLink" className="nav-link" href="/about">About</a>
+            <a className="nav-link" href="./About">About</a>
           </li>
         </ul>
-        
-        <div className="collapse navbar-collapse" id="navbarSupportedContent15">
-      
-          
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="./Home">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="./Calendar">Calendar</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="./Excuses">Excuses</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="./About">About</a>
-            </li>
-          </ul>
-          
-        </div>
-        
-      </nav>
-     
- )   
- 
+
+      </div>
+
+    </nav>
+
+  )
+
 }
