bd65e218310204aa151a15f4ca7f9f30a99282ab
chbbs231 <atrobles231@gmail.com>
2019-12-03 19:46:22 -0800

    incoming

--- views/index.jsx
+++ views/index.jsx
@@ -6,6 +6,7 @@ module.exports = props => {
     return(
     <Layout>
     <img src= "https://files.slack.com/files-pri/TKAFG5QD9-FR0A109TN/flakelogo.png"></img>
+    
     <button  id = "customBtn" type="button" class="btn btn-gplus"><i class="fab fa-google-plus-g pr-1"></i> Google +</button>
     </Layout>
     )
