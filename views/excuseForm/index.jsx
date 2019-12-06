17dc886f8eba033e478886f4a929ee553ca295d0
Mercedes Walsh <sadieswalsh@gmail.com>
2019-12-05 21:17:38 -0800

    added className on views and added slider excuses

--- views/excuseForm/index.jsx
+++ views/excuseForm/index.jsx
@@ -11,6 +11,22 @@ module.exports = props => {
       <form>
         <p>
           <h4>|| Generate random excuse ||</h4>
+          <div className="md-form">
+          <input type="text" id="randomExcuse" className="form-control mdb-autocomplete" />
+          <label htmlFor="randomExcuse" className="active">What is your excuse?</label>
+        </div>
+
+        <form className="range-field my-4 w-25">
+          <label htmlFor="severity">How intense of an excuse do you want?</label>
+          <div className="row justify-content-between">
+            <div className="col-1 slider1" id="randomOne">1</div>
+            <div className="col-1 slider" id="randomTwo">2</div>
+            <div className="col-1 slider" id="randomThree">3</div>
+            <div className="col-1 slider" id="randomFour">4</div>
+            <div className="col-1 slider5" id="randomFive">5</div>
+          </div>
+          <input type="range" className="custom-range" min="1" max="5" id="randomSeverity" />
+        </form>
           <button className="btn peach-gradient" id="randomBtn">GENERATE</button>
         </p>
 
@@ -23,13 +39,13 @@ module.exports = props => {
         </div>
 
         <form className="range-field my-4 w-25">
-          <label htmlFor="severity">How intense is your excuse?</label>
+          <label htmlFor="severity">How intense is the excuse?</label>
           <div className="row justify-content-between">
-            <div className="col-1 slider1" id="oneIntense">1</div>
-            <div className="col-1 slider" id="twoIntense">2</div>
-            <div className="col-1 slider" id="threeIntense">3</div>
-            <div className="col-1 slider" id="fourIntense">4</div>
-            <div className="col-1 slider5" id="fiveIntense">5</div>
+            <div className="col-1 slider1" id="userOne">1</div>
+            <div className="col-1 slider" id="userTwo">2</div>
+            <div className="col-1 slider" id="userThree">3</div>
+            <div className="col-1 slider" id="userFour">4</div>
+            <div className="col-1 slider5" id="userFive">5</div>
           </div>
           <input type="range" className="custom-range" min="1" max="5" id="severity" />
         </form>
