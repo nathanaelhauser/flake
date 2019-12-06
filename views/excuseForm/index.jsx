638ab479f449ca29b75f03bc5128355d3149e8a2
Mercedes Walsh <sadieswalsh@gmail.com>
2019-12-05 19:28:45 -0800

    edited views for excuse form

--- views/excuseForm/index.jsx
+++ views/excuseForm/index.jsx
@@ -13,8 +13,10 @@ module.exports = props => {
           <h4>|| Generate random excuse ||</h4>
           <button className="btn peach-gradient" id="randomBtn">GENERATE</button>
         </p>
+
         <hr/> 
-        <h4>|| Add an excuse to an event ||</h4>
+
+          <h4>|| Add an excuse to an event ||</h4>
         <div className="md-form">
           <input type="text" id="excuse" className="form-control mdb-autocomplete" />
           <label htmlFor="excuse" className="active">What is your excuse?</label>
@@ -23,24 +25,24 @@ module.exports = props => {
         <form className="range-field my-4 w-25">
           <label htmlFor="severity">How intense is your excuse?</label>
           <div className="row justify-content-between">
-            <div className="col-1 slider1">1</div>
-            <div className="col-1 slider">2</div>
-            <div className="col-1 slider">3</div>
-            <div className="col-1 slider">4</div>
-            <div className="col-1 slider5">5</div>
+            <div className="col-1 slider1" id="oneIntense">1</div>
+            <div className="col-1 slider" id="twoIntense">2</div>
+            <div className="col-1 slider" id="threeIntense">3</div>
+            <div className="col-1 slider" id="fourIntense">4</div>
+            <div className="col-1 slider5" id="fiveIntense">5</div>
           </div>
           <input type="range" className="custom-range" min="1" max="5" id="severity" />
         </form>
         <button className="btn peach-gradient" id="addExcuse">Add excuse to an event</button>
       </form>
-      <hr/>
 
+      <hr/>
 
       <div className="row">
         <div>
         <table>
           <thead>
-            <th> Recent excuses</th>
+            <th>Recent excuses</th>
             <br/><br/>
             <th></th>
           </thead>
