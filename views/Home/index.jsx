498f5887dc6146b02d40b81ee2d1277d3887b72b
chbbs231 <atrobles231@gmail.com>
2019-12-05 18:44:25 -0800

    fixed errors

--- views/Home/index.jsx
+++ views/Home/index.jsx
@@ -5,10 +5,10 @@ const Footer = require('../Footer')
 
 module.exports = props => {
   return (
-    <Layout>
-      <Navbar />
-      <div className="container">
-        <br />
+   <Layout>
+     <Navbar/>
+     <div className= "container">
+        <br/>
         {/* Use google username to greet client */}
         <h1>Hello {props.username}! What would You like to do today?</h1>
         <div className="row">
@@ -17,17 +17,18 @@ module.exports = props => {
 
             <div className="card-body">
               <h5 className="card-title">Create Excuse</h5>
-              <a href="http://localhost:3000/Excuses" className="stretched-link">Go somewhere</a>
+              <a href="http://localhost:3000/Excuses" className="stretched-link">Let's Flake!/a>
             </div>
           </div>
         </div>
+        <br></br>
           <div className="col-md-6"><div className="card">
             <img className="card-img-top" src="https://images.idgesg.net/images/article/2019/04/google-calendar-android-100794956-large.jpg" alt="calpic" />
 
             <div className="card-body">
               <h5 className="card-title">View Upcoming Events</h5>
 
-              <a href="http://localhost:3000/Calendar" className="stretched-link">Go somewhere</a>
+              <a href="http://localhost:3000/Calendar" className="stretched-link">Let's See What were Skipping</a>
             </div>
           </div></div>
 
@@ -35,8 +36,10 @@ module.exports = props => {
       </div>
       {/* Passing google_id to client using empty div */}
       <div id="tempStorage" data-google_id={props.google_id}></div>
-      <script src="./home.js" />
-    </Layout>
+
+      <script src="./home.js"/>
+<     Footer/>
+   </Layout>
 
   )
 }
