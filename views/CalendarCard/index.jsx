5150bf6a7cf3e2345cfb054a1c467afbbf38afc0
chbbs231 <atrobles231@gmail.com>
2019-12-05 20:43:25 -0800

    pics now show

--- views/CalendarCard/index.jsx
+++ views/CalendarCard/index.jsx
@@ -2,19 +2,19 @@ const React = require(`react`)
 
 module.exports = props => {
   return(
-        <div className="container">
-        <div className="card card-cascade wider">
-            <div className="view view-cascade gradient-card-header peach-gradient">
-                <h2 className="card-header-title mb-3">{props.event}</h2>
-                    <p className="mb-0">{props.eventDate}</p>
-            </div>
-            <div className="card-body card-body-cascade text-center">
-                <p className="card-text">{props.excuse}</p>
-                <a href="#!" class="orange-text d-flex flex-row-reverse p-2">
-                <h5 className="waves-effect waves-light">Add an excuse<i class="fas fa-angle-double-right ml-2"></i></h5>
-                </a>
-            </div>
+    <div className="container">
+    <div className="card card-cascade wider">
+        <div className="view view-cascade gradient-card-header peach-gradient">
+            <h2 className="card-header-title mb-3">{props.event}</h2>
+                <p className="mb-0">{props.eventDate}</p>
         </div>
+        <div className="card-body card-body-cascade text-center">
+            <p className="card-text">{props.excuse}</p>
+            <a href="#!" class="orange-text d-flex flex-row-reverse p-2">
+            <h5 className="waves-effect waves-light">Add an excuse<i class="fas fa-angle-double-right ml-2"></i></h5>
+            </a>
         </div>
+    </div>
+    </div> 
   )
 }
--- views/index.jsx
+++ views/index.jsx
@@ -4,8 +4,9 @@ const Layout = require ('./Layout2')
 module.exports = props => {
     return(
       <Layout>
-
+        
          <button  id = "customBtn" type="button" className="btn btn-gplus"><i className="fab fa-google-plus-g pr-1"></i> Google +</button>
+
         <script src="./index.js" />
       </Layout>
 
