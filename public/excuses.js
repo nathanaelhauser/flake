08b86a152c8815ae592b8441fadf7d6042b4bf1d
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-12-07 10:29:12 -0800

    Adding functionality to excuse page

--- public/excuses.js
+++ public/excuses.js
@@ -1,5 +1,21 @@
-document.getElementById('randomBtn').addEventListener('click', e => {
-  e.preventDefault()
-  
-  axios.get('/excuses')
+document.getElementById('randomForm').style.display = 'block'
+document.getElementById('randomConfirm').style.display = 'none'
+
+document.querySelector('body').addEventListener('click', e => {
+
+  if (e.target.id === 'randomBtn') {
+    e.preventDefault()
+    console.log('hello')
+    // document.getElementById('randomForm').style.display = 'none'
+    // document.getElementById('randomConfirm').style.display = 'block'
+
+    const severity = document.getElementById('')
+
+    axios.get('/excuses')
+      .then(excuses => {
+        console.log(excuses)
+      })
+      .catch(e => console.log(e))
+  }
+
 })
