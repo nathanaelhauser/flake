3464fe52861c403bfdbe7baf76b20072beb033a5
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-12-08 02:52:19 -0800

    Cleaned excuses.js and add random excuse to event works

--- public/excuses.js
+++ public/excuses.js
@@ -1,24 +1,25 @@
 let eventsArr = []
+let excuse_id
 
 document.getElementById('randomForm').style.display = 'block'
 document.getElementById('randomConfirm').style.display = 'none'
 
-axios.get(`/google/calendars/${localStorage.getItem('google_id')}`)
-  .then(({ data }) => {
-    data.forEach(calendar => {
-      document.querySelector('#selectCalendar').innerHTML +=
-        `<option value=${calendar.id}>${calendar.summary}</option>`
-    })
-  })
-  .catch(e => console.log(e))
+const createEvent = event => {
+  axios.post('/events', event)
+    .then(() => window.location = '/Calendar')
+    .catch(e => console.error(e))
+}
 
+// All click events
 document.querySelector('body').addEventListener('click', e => {
 
+  // When client chooses a calendar
   if (e.target.id === 'selectCalendar') {
     const calID = document.getElementById('selectCalendar').value
     if (calID !== 'Choose Calendar') {
       axios.get(`/google/events/${localStorage.getItem('google_id')}/${calID}`)
         .then(({ data }) => {
+          console.log(data)
           document.getElementById('selectEvent').innerHTML = '<option selected>Choose Event</option>'
           eventsArr = []
           data.forEach((event, i) => {
@@ -35,20 +36,23 @@ document.querySelector('body').addEventListener('click', e => {
     }
   }
 
+  // Grab a random excuse for the client
   if (e.target.id === 'randomBtn') {
     e.preventDefault()
-    let excuseID = ''
 
     axios.get(`/excuses/${document.getElementById('randomSeverity').value}`)
       .then(({ data }) => {
-        excuseID = data.id
+        const randomIndex = Math.floor(Math.random()*(data.length - 1))
         document.getElementById('randomConfirm').style.display = 'block'
-        document.getElementById('randomExcuse').textContent =
-          data[Math.floor(Math.random() * (data.length - 1))].excuse
+        document.getElementById('randomExcuse').textContent = data[randomIndex].excuse
+        excuse_id = data[randomIndex].id
+        console.log(data)
+        console.log(excuse_id)
       })
       .catch(e => console.error(e))
   }
 
+  // Add a new excuse to db and to event
   if (e.target.id === 'addBtn') {
     e.preventDefault()
     console.log('addBtn')
@@ -56,7 +60,8 @@ document.querySelector('body').addEventListener('click', e => {
     const calendar_id = document.getElementById('selectCalendar').value
     const index = document.getElementById('selectEvent').value
 
-    if (calendar_id !== 'Choose Calendar' && index !== 'Choose Event' 
+    if (calendar_id !== 'Choose Calendar' 
+      && index !== 'Choose Event' 
       && document.getElementById('newExcuse').value) {
         console.log('post excuse')
         axios.post('/excuses', {
@@ -64,8 +69,7 @@ document.querySelector('body').addEventListener('click', e => {
           excuse: document.getElementById('newExcuse').value
         })
           .then(({ data }) => {
-            console.log(data)
-            axios.post('/events', {
+            createEvent({
               user_id: localStorage.getItem('google_id'),
               excuse_id: data.id,
               start: eventsArr[index].start,
@@ -73,24 +77,40 @@ document.querySelector('body').addEventListener('click', e => {
               calendar_id,
               event: eventsArr[index].summary
             })
-              .then(({ data }) => {
-                console.log('post event')
-              })
-              .catch(e => console.error(e))
           })
           .catch(e => console.error(e))
     }
   }
 
-  if (e.target.id === 'addRandom') {
+  // Add a random excuse to event
+  if (e.target.id === 'randomAdd') {
     e.preventDefault()
 
-    const calendar = document.getElementById('selectCalendar').value
-    const event = document.getElementById('selectEvent').value
+    const calendar_id = document.getElementById('selectCalendar').value
+    const index = document.getElementById('selectEvent').value
 
-    if (calendar !== 'Choose Calendar' && event !== 'Choose Event' && document.getElementById('randomExcuse').textContent) {
-      
+    if (calendar_id !== 'Choose Calendar' 
+      && index !== 'Choose Event' 
+      && excuse_id) {
+        createEvent({
+          user_id: localStorage.getItem('google_id'),
+          excuse_id,
+          start: eventsArr[index].start,
+          end: eventsArr[index].end,
+          calendar_id,
+          event: eventsArr[index].summary
+        })
     }
   }
 
 })
+
+// Grab client calendars from Google Calendar
+axios.get(`/google/calendars/${localStorage.getItem('google_id')}`)
+  .then(({ data }) => {
+    data.forEach(calendar => {
+      document.querySelector('#selectCalendar').innerHTML +=
+        `<option value=${calendar.id}>${calendar.summary}</option>`
+    })
+  })
+  .catch(e => console.log(e))
