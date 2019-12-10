3b4312ca1ccbc2c0403486c2b8449dcf7986a873
yokevi <you.k2208@gmail.com>
2019-12-09 21:32:16 -0800

    btn font = black, added btns to home  & clck event

--- public/home.js
+++ public/home.js
@@ -7,5 +7,14 @@ if (!localStorage.getItem('google_id')) {
   localStorage.setItem('username', data.username)
 }
 
-document.getElementById('greeting').textContent = `Hello ${localStorage.getItem('username')}! What would you like to do today?`
+document.getElementById('greeting').textContent = `Hello ${localStorage.getItem('username')}! Let's plan your next flake!`
+
+// event listener for Excuses and Events buttons
+document.addEventListener(`click`, e => {
+  if (e.target.id === `excuseBtn`) {
+    location.href = `/Excuses`
+  } else if (e.target.id === `eventBtn`) {
+    location.href = `/Calendar`
+  }
+})
 
--- views/CalendarCard/index.jsx
+++ views/CalendarCard/index.jsx
@@ -3,10 +3,10 @@ const React = require(`react`)
 module.exports = props => {
   return(
         <div className="container">
-        <div className="card card-cascade wider">
-            <div className="view view-cascade gradient-card-header peach-gradient">
-                <h2 className="card-header-title mb-3">{props.event}</h2>
-                    <p className="mb-0">{props.eventDate}</p>
+        <div className="card text-center">
+            <div className="card-header peach-gradient h2">
+                {props.event}
+                <p className="mb-0">{props.eventDate}</p>
             </div>
             <div className="card-body card-body-cascade text-center">
                 <div id="eventList" className="list-group"></div>
