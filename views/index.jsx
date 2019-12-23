76dfedca622f39cedb5248aed18586016f33d788
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-12-23 11:17:46 -0800

    Fixed index background color

--- views/index.jsx
+++ views/index.jsx
@@ -4,11 +4,13 @@ const Layout = require('./Layout2')
 module.exports = props => {
   return (
     <Layout>
-      <div id="index-container" className="container d-flex peach-gradient">
-        <div className="card align-middle">
-          <img id="index-logo" className="card-img-top" src="./assets/logo_index.png" alt="logo" />
-          <div className="card-body text-center">
-            <button id="signin" type="button" className="btn btn-gplus"><i className="fab fa-google-plus-g pr-1"></i> Google +</button>
+      <div className="background-color peach-gradient">
+        <div id="index-container" className="container d-flex">
+          <div className="card align-middle">
+            <img id="index-logo" className="card-img-top" src="./assets/logo_index.png" alt="logo" />
+            <div className="card-body text-center">
+              <button id="signin" type="button" className="btn btn-gplus"><i className="fab fa-google-plus-g pr-1"></i> Google +</button>
+            </div>
           </div>
         </div>
       </div>
